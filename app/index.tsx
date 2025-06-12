abefbae3d99e5ac3ee0ceb90b4192ebdcd1f30cd
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-06-12 11:28:56 +0530

    CleanUp completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/auth.styles";
 
 export default function Index () {
     return(
         <View
-            style={{
-                flex:1,
-                justifyContent: "center",
-                alignItems: "center"
-            }}
+            style={styles.container}
         >
-            <Text>Edit App/index.tsx to edit the screen.</Text>
+            <Text
+                style = {styles.title}
+            >
+                Hello
+            </Text>
+            <TouchableOpacity onPress = {()=>alert("you touched")}>
+                <Text>Press Me</Text>
+            </TouchableOpacity>
+            <Pressable onPress={()=>alert("You touched pressable")}>
+                <Text>Press me - Pressable</Text>
+            </Pressable>
         </View>
     )
 }
+
