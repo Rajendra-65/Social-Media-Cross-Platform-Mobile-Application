ff93140668c4e025ad043b121444e5b0bde3d3d8
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-06-12 11:09:56 +0530

    Added the starter code for Spotlight App

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index () {
+    return(
+        <View
+            style={{
+                flex:1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+        >
+            <Text>Edit App/index.tsx to edit the screen.</Text>
+        </View>
+    )
+}
